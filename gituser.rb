c2282a744f1d1873d39153a4ed6c28e65a1958cd
lucasnevespereira <pereiraneveslucas@gmail.com>
2023-08-22 00:47:42 +0200

    Brew formula update for gituser version v1.3.6

--- gituser.rb
+++ gituser.rb
@@ -5,11 +5,11 @@
 class Gituser < Formula
   desc ""
   homepage "https://github.com/lucasnevespereira/homebrew-tools"
-  version "1.3.5"
+  version "1.3.6"
 
   on_macos do
-    url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.3.5/gituser_1.3.5_darwin_all.tar.gz"
-    sha256 "f6e46c80b434271e30998319a751e0a0df115afb13c8d4509b1a76671b3db50b"
+    url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.3.6/gituser_1.3.6_darwin_all.tar.gz"
+    sha256 "3422a29da893fe9d3373d23d4f8bfb7322c4ca071ad440308ee6917d6360865c"
 
     def install
       bin.install "gituser"
@@ -18,16 +18,16 @@ class Gituser < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.3.5/gituser_1.3.5_linux_amd64.tar.gz"
-      sha256 "4e9145441f8614fcede992cb875f3d5422e08e11e47e0fcc7abbd7a588517e36"
+      url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.3.6/gituser_1.3.6_linux_amd64.tar.gz"
+      sha256 "ee721aceaf66c6175f7a710a569a216ca0f0e28cfee276b8a6b5805faf1cee0e"
 
       def install
         bin.install "gituser"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.3.5/gituser_1.3.5_linux_arm64.tar.gz"
-      sha256 "21f3641dfa154a2cbbaa935dd6752178e6d580ba2fc6e8b4663cf5edeabdb618"
+      url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.3.6/gituser_1.3.6_linux_arm64.tar.gz"
+      sha256 "35ccf5da1b3c5a0004031ae77549582085f8fec376f86cb22f595b15556bed74"
 
       def install
         bin.install "gituser"
