392024c12e4ca305dc8a83a3a3a01a59f5b8d822
lucasnevespereira <pereiraneveslucas@gmail.com>
2026-04-26 16:59:02 +0200

    use build-from-source formula like logmx

--- gituser.rb
+++ gituser.rb
@@ -1,36 +1,18 @@
-# typed: false
-# frozen_string_literal: true
-
 class Gituser < Formula
   desc "Switch between git accounts easily"
   homepage "https://github.com/lucasnevespereira/go-gituser"
-  version "1.5.4"
+  url "https://github.com/lucasnevespereira/go-gituser/archive/refs/tags/vVERSION.tar.gz"
+  sha256 "NEED_SHA256"
+  license "MIT"
 
-  on_macos do
-    url "https://github.com/lucasnevespereira/go-gituser/archive/refs/tags/v1.5.4.tar.gz"
-    sha256 "64b666ef7a441bc20e384690146c4a6abfc86d96cacaf57af5e4150154de7871"
+  depends_on "go" => :build
 
-    def install
-      bin.install "gituser"
-    end
+  def install
+    ldflags = "-s -w -X github.com/lucasnevespereira/go-gituser/cmd.AppVersion=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "."
   end
 
-  on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.5.4-beta.1/gituser-linux-arm64.tar.gz"
-      sha256 "ffc6c8f13f63d488c4537b4267ea75ab66a18d1b1aa0c5a2fc23aee98e889b1a"
-
-      def install
-        bin.install "gituser"
-      end
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/lucasnevespereira/go-gituser/releases/download/v1.5.4-beta.1/gituser-linux-amd64.tar.gz"
-      sha256 "36fbea62249f681413be8b7bd59928874abb201d3af6adfcc3ac84a551c09045"
-
-      def install
-        bin.install "gituser"
-      end
-    end
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gituser --version")
   end
 end
