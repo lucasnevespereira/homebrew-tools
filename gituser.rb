6aa82d3cb27c638fd1bead7455a2c0cbd31bd138
lucasnevespereira <pereiraneveslucas@gmail.com>
2026-04-26 17:08:25 +0200

    gituser: seed formula with v1.5.4 url and sha256

--- gituser.rb
+++ gituser.rb
@@ -1,8 +1,8 @@
 class Gituser < Formula
   desc "Switch between git accounts easily"
   homepage "https://github.com/lucasnevespereira/go-gituser"
-  url "https://github.com/lucasnevespereira/go-gituser/archive/refs/tags/vVERSION.tar.gz"
-  sha256 "NEED_SHA256"
+  url "https://github.com/lucasnevespereira/go-gituser/archive/refs/tags/v1.5.4.tar.gz"
+  sha256 "64b666ef7a441bc20e384690146c4a6abfc86d96cacaf57af5e4150154de7871"
   license "MIT"
 
   depends_on "go" => :build
